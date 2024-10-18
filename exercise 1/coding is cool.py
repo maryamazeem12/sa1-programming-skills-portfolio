800d6a4a695df0071e8fb4d76d08cf0e9cec8a58
Taha226238 <144995443+Taha226238@users.noreply.github.com>
2024-10-18 13:51:11 +0400

    Create coding is cool.py
    coding school

---
+++ exercise 1/coding is cool.py
@@ -0,0 +1,12 @@
+# Assign word1 to the first word
+word1 = "Coding" 
+
+# Assign word2 to the second word
+word2 = "is"
+
+# Assign word3 to the third word
+word3 = "Cool"
+
+# The variables are combined with spaces in between using string concatenation
+# This will print the phrase 'Coding is cool'as the output
+print(word1 + " " + word2 + " " + word3)
