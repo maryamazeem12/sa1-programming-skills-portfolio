e96412e28d10f88cc70890d425600177e13de679
maryamazeem12 <mariumazeem954@gmail.com>
2024-10-25 15:36:58 +0400

    exercise 2
     simple sums

---
+++ exercise 2/simple sums.py
@@ -0,0 +1,17 @@
+#step1 declare a variable and initialize it with the integer value 8.
+
+#Ahmed has 8 cats
+ahmed = int(8)
+
+#step2 declare a variable and initiliaze it with the integar value 10
+
+#furqan has 10 cats
+furqan = int(10)
+
+#step3 a third variable that stores the sum of first two numbers.
+
+# sum number of cats both have
+sum =  ahmed + furqan 
+
+#step4 print the sum
+print(sum)
