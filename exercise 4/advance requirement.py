c14ad5e4dc92eaf36a93cc7860a07850d5b23400
maryamazeem12 <mariumazeem954@gmail.com>
2024-11-02 11:35:01 +0400

    exercise 4:- primitive quiz

---
+++ exercise 4/advance requirement.py
@@ -0,0 +1,190 @@
+#quiz that asks for the capitals of 10 European countries.Provide feedback for each question
+
+#the lists of 10 european countries and their capitals
+
+points = 0
+#using nested decision structure to determine the capital of (BELGIUM)
+#get input from user 
+capital = (input("what is the capital of belgium?"))
+
+#check if the user answer is correct
+if capital.lower () == "brussels" :
+#print the message if user answer is correct
+    print("CORRECT! WELLDONE")
+    points += 1
+
+#print the message if user answer is incorrect
+else :
+    print(f"WRONG\nThe correct answer is brussels.")
+
+
+
+#using nested decision structure to determine the capital of (BULGARIA)
+#get input from user 
+capital = (input("what is the capital of bulgaria?"))
+
+#check if the user answer is correct
+if capital.lower () == "sofia" :
+#print the message if user answer is correct
+    print("CORRECT! WELLDONE")
+    points += 1
+
+#print the message if user answer is incorrect
+else :
+    print(f"WRONG\nThe correct answer is sofia.")
+    
+
+
+#using nested decision structure to determine the capital of (GERMANY)
+#get input from user 
+capital = (input("what is the capital of germany?"))
+
+#check if the user answer is correct
+if capital.lower () == "berlin" :
+#print the message if user answer is correct
+    print("CORRECT! WELLDONE")
+    points += 1
+    
+#print the message if user answer is incorrect
+else :
+    print(f"WRONG\nThe correct answer is berlin.")
+    
+
+
+#using nested decision structure to determine the capital of (UK)
+#get input from user 
+capital = (input("what is the capital of united kingdom?"))
+
+#check if the user answer is correct
+if capital.lower () == "london" :
+#print the message if user answer is correct
+    print("CORRECT! WELLDONE")
+    points += 1
+
+#print the message if user answer is incorrect
+else :
+    print(f"WRONG\nThe correct answer is london.")
+
+
+
+#using nested decision structure to determine the capital of (ITALY)
+#get input from user 
+capital = (input("what is the capital of italy?"))
+
+#check if the user answer is correct
+if capital.lower () == "rome" :
+#print the message if user answer is correct
+    print("KEEP GOING! WELLDONE ")
+    points += 1
+    
+#print the message if user answer is incorrect
+else :
+    print(f"WRONG\nThe correct answer is rome.")
+    
+
+
+
+#using nested decision structure to determine the capital of (SPAIN)
+#get input from user 
+capital = (input("what is the capital of spain?"))
+
+#check if the user answer is correct
+if capital.lower () == "madrid" :
+#print the message if user answer is correct
+    print("CORRECT! SUPERB")
+    points += 1
+
+#print the message if user answer is incorrect
+else :
+    print(f"WRONG\nThe correct answer is madrid.")
+   
+
+
+
+#using nested decision structure to determine the capital of (PORTUGAL)
+#get input from user 
+capital = (input("what is the capital of portugal?"))
+
+#check if the user answer is correct
+if capital.lower () == "lisbon" :
+#print the message if user answer is correct
+    print("CORRECT! WONDERFUL ")
+    points += 1
+
+#print the message if user answer is incorrect
+else :
+    print(f"WRONG\nThe correct answer is lisbon.")
+    
+
+
+
+#using nested decision structure to determine the capital of (RUSSIA)
+#get input from user 
+capital = (input("what is the capital of russia?"))
+
+#check if the user answer is correct
+if capital.lower () == "moscow" :
+#print the message if user answer is correct
+    print(" CORRECT!")
+    points += 1
+
+#print the message if user answer is incorrect
+else :
+    print(f"WRONG\nThe correct answer is moscow.")
+
+
+
+
+#using nested decision structure to determine the capital of (TURKEY)
+#get input from user 
+capital = (input("what is the capital of turkey?"))
+
+#check if the user answer is correct
+if capital.lower () == "ankara" :
+#print the message if user answer is correct
+    print("WELLDONE! KEEP GOING ")
+    points += 1
+
+#print the message if user answer is incorrect
+else :
+    print(f"WRONG\nThe correct answer is ankara.")
+    
+
+
+
+#using nested decision structure to determine the capital of (SWITZERLAND)
+#get input from user 
+capital = (input("what is the capital of switzerland?"))
+
+#check if the user answer is correct
+if capital.lower () == "bern" :
+#print the message if user answer is correct
+    print("CORRECT! AMAZING ")
+    points += 1
+
+#print the message if user answer is incorrect
+else :
+    print(f"WRONG\nThe correct answer is bern.")
+    
+
+
+
+#final score of the quiz
+print(f"you got {points}/10 correct.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
