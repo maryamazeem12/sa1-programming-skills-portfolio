cf1b71d0c2fe436b6ae506d2885183381b641194
maryamazeem12 <mariumazeem954@gmail.com>
2024-11-13 19:58:46 +0400

    Biography.py

--- exercise 3/Biography.py
+++ exercise 3/Biography.py
@@ -1,3 +1,4 @@
+#In this exercise, you’ll create a program that stores and prints your name, home-town, and age to the console using a Python dictionary.
 1.# Store the information in dictionary
 Information = {'name' : 'Maryam '  , 
            'hometown' : 'karachi' ,
