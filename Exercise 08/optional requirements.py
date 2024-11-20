3cdfd5ef2dc12d48b588922edca5ec3647b5e104
maryamazeem12 <mariumazeem954@gmail.com>
2024-11-20 18:46:12 +0400

    optional requirements.py

--- Exercise 08/optional requirements.py
+++ Exercise 08/optional requirements.py
@@ -2,16 +2,15 @@
 #Implement the search functionality based on user input.
 
 
-# Initializing the name list
-name_list2 = ["jake","zac","ion","ron","sam","dave"] 
+#Initializing the name list 
+name_list2 = ["jake","zac","ion","ron","sam","dave"]
 
-# Asking the user to enter the name
-search_name = input("Which name do you want to search for? ")
+#asking the user to enter a name
+search_name = input("enter the name you want to search for?").lower()
 
-# Using an if-else statement to check if the name is in the list
-if search_name in name_list2:
-    print(f"{search_name} was found in the list.")
+#using if else statement to check if the name is in the lists
+if search_name in [name.lower() for name in name_list2] :
+    print(f"{search_name} was found in the list")
 else:
-    print(f"{search_name} was not found in the list.")
-
+    print(f"{search_name}was not found in the list")
 
