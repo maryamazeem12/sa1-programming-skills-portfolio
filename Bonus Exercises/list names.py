2f375f57d44bdc1adb09bde9261aecc657ddf8f0
maryamazeem12 <mariumazeem954@gmail.com>
2024-11-20 14:28:40 +0400

    Delete Bonus Exercises directory

--- Bonus Exercises/list names.py
+++
@@ -1,7 +0,0 @@
-#Store the names of a few of your friends in a list called names. Print each person’s name by accessing each element in the list, one at a time.
-
-#Store your friends names in the lists
-friend_names = ["aneeka","asna","farah","sana","aisha","nehdia"]
-
-#printing each person's names one at a time
-print(*friend_names, sep='\n') 
