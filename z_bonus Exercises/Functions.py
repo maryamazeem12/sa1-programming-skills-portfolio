d3a125bd7984fec6db1eb0ade1ce9fc2358c5692
maryamazeem12 <mariumazeem954@gmail.com>
2024-11-20 14:26:52 +0400

    z_bonus Exercises

---
+++ z_bonus Exercises/Functions.py
@@ -0,0 +1,28 @@
+#Write a function called favorite_book() that accepts one parameter, title. The function should print a message, such as One of my favorite books is Alice in Wonderland.
+# Call the function, making sure to include a book title as an argument in the function call.
+
+
+#assigning parameter {title} to function {favorite book}
+def favorite_book(title) :   #title is the parameter
+    print(f"One of my favorite book is {title}") 
+
+
+#print a message telling your favorite book
+book = " Alice in Wonderland."
+#call the fucntion
+favorite_book(book)    #book title is the arguemnt in the funtion 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ z_bonus Exercises/Variable.py
@@ -0,0 +1,28 @@
+#Assign a message to a variable, and print that message. Then change the value of the variable to a new message, and print the new message.
+
+#Assigning a message to the variable
+message = "hi,everyone"
+
+#Now,print the message
+print(message)
+
+#Assigning a new message to the variable
+new_message = "i am maryam"
+ 
+#Now,print the new message
+print(new_message)
+
+
+#Variables (Favorite Number):
+#Use a variable to represent your favorite number. Then, using that variable, create a message that reveals your favorite number,Print that message.
+
+#Assign a variable to your favorite number,my favorite number is 18
+favorite_number = "18"
+
+#provide a message_2 to print your favourite number
+message_2  = (f"My favorite number is {favorite_number} ")
+
+#print the message to get my favorite number as output
+print(message_2)
+
+
