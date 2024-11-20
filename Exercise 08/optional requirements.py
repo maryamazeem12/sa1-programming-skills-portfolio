58d98001c6657a0aca37f58e8d8835e72a9156cc
maryamazeem12 <mariumazeem954@gmail.com>
2024-11-20 14:57:15 +0400

    optional requirements.py

--- Exercise 08/optional requirements.py
+++ Exercise 08/optional requirements.py
@@ -15,14 +15,3 @@ else:
     print(f"{search_name} was not found in the list.")
 
 
-#Initializing the name list 
-name_list2 = ["jake","zac","ion","ron","sam","dave"]
-
-#asking the user to enter a name
-search_name = input("enter the name you want to search for?")
-
-#using if else statement to check if the name is in the list
-if search_name in name_list2:
-    print(f"{search_name} was found in the list")
-else:
-    print(f"{search_name}was not found in the list")
